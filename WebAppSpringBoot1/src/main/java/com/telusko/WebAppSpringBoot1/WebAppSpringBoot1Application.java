c72ab6fe3ac7d97aef8cee1bbfe1e78826b4f693
Nilofaur <74742249+Nilofaur@users.noreply.github.com>
2025-03-13 14:09:46 +0000

    Add files via upload

---
+++ WebAppSpringBoot1/src/main/java/com/telusko/WebAppSpringBoot1/WebAppSpringBoot1Application.java
@@ -0,0 +1,13 @@
+package com.telusko.WebAppSpringBoot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppSpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppSpringBoot1Application.class, args);
+	}
+
+}
